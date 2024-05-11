9d3e1d9dcfa30466bb155d9c852e40223757b315
liuhean <530380237@qq.com>
2024-05-11 23:24:13 +0800

    增加run文件

---
+++ run.py
@@ -0,0 +1,32 @@
+# 写在顶部
+from vnpy_datamanager import DataManagerApp
+from vnpy_datarecorder import DataRecorderApp
+from vnpy.event import EventEngine
+from vnpy.trader.engine import MainEngine
+from vnpy.trader.ui import MainWindow, create_qapp
+
+from vnpy_ctp import CtpGateway
+from vnpy_ctastrategy import CtaStrategyApp
+from vnpy_ctabacktester import CtaBacktesterApp
+
+
+def main():
+    """Start VeighNa Trader"""
+    qapp = create_qapp()
+
+    event_engine = EventEngine()
+    main_engine = MainEngine(event_engine)
+
+    main_engine.add_gateway(CtpGateway)
+    main_engine.add_app(CtaStrategyApp)
+    main_engine.add_app(CtaBacktesterApp)
+    main_engine.add_app(DataManagerApp)
+    main_engine.add_app(DataRecorderApp)
+    main_window = MainWindow(main_engine, event_engine)
+    main_window.showMaximized()
+
+    qapp.exec()
+
+
+if __name__ == "__main__":
+    main()
